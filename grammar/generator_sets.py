65df2c26eda8ea74a731d04757db31a54dfb4dbd
Irina Tomic <irina.z.tomic@gmail.com>
2023-12-08 01:43:49 +0100

    Complete first & follow sets generator - LR tables

---
+++ grammar/generator_sets.py
@@ -0,0 +1,93 @@
+# FIRST SETS
+def generateFirstSets(G, T, nT):
+
+    first_sets = {symbol: set() for symbol in nT}
+    epsilon = '$'
+    changed = True               # track changes in the FIRST sets
+
+    # Until no changes
+    while changed:
+        changed = False  
+
+        for rule in G:
+            lhs, rhs = rule[1], rule[2].split(' ')  
+            if lhs not in nT:           # extra check
+                continue
+
+            # go through production (rhs)
+            for symbol in rhs:
+                if symbol in nT:
+                    # Iterate through the FIRST set of the symbol -> nadskup
+                    for term in first_sets[symbol]:
+                        if term != epsilon and term not in first_sets[lhs]:
+                            first_sets[lhs].add(term)
+                            changed = True                           # Changes occur
+                    
+                    # If epsilon is not in the FIRST set of the symbol, break the loop
+                    if epsilon not in first_sets[symbol]:
+                        break  
+
+                # Non-terminal
+                elif symbol != epsilon:
+                    # Add the symbol to the FIRST set of the left-hand side
+                    if symbol not in first_sets[lhs]:
+                        first_sets[lhs].add(symbol)
+                        changed = True                              # Changes occur
+                    break                                           # Break to handle the next rule
+            
+             # If the loop completes without breaking, add epsilon to the FIRST set of the left-hand side
+            else:
+                first_sets[lhs].add(epsilon)
+                changed = True                                      # Changes occur           
+
+    return first_sets
+
+# FOLLOW SETS
+def generateFollowSets(G, T, nT):
+    follow_sets = {symbol: set() for symbol in nT}
+    epsilon = '$'       # Representing end of input (added as a terminal)
+
+    changed = True      # track changes in the FOLLOW sets
+    while changed:
+        changed = False
+
+        for rule in G:
+            lhs, rhs = rule[1], rule[2].split(' ')
+            for i, symbol in enumerate(rhs):
+                if symbol in nT:
+                    next_symbol = None
+
+                     # Traverse the symbols to the right of the current non-terminal
+                    for j in range(i + 1, len(rhs)):
+                        next_symbol = rhs[j]
+
+                        # If it's a non-terminal, update its FOLLOW set based on the current non-terminal
+                        if next_symbol in nT:
+                            if epsilon not in follow_sets[next_symbol] and next_symbol != lhs:
+                                follow_sets[next_symbol].add(epsilon)
+                                changed = True
+
+                             # Break loop when encountering a non-terminal symbol
+                            break
+
+                        # If it's a terminal, add it to the FOLLOW set and break
+                        elif next_symbol in T:
+                            if next_symbol not in follow_sets[symbol]:
+                                follow_sets[symbol].add(next_symbol)
+                                changed = True
+                            break
+                    
+                    # If the loop completes without breaking, update the FOLLOW set with the current non-terminal's FOLLOW set
+                    else:
+                        if lhs != symbol:
+                            for term in follow_sets[lhs]:
+                                if term not in follow_sets[symbol]:
+                                    follow_sets[symbol].add(term)
+                                    changed = True
+
+        # Adding the # symbol to the FOLLOW set of the start symbol
+        if '#' not in follow_sets[nT[0]]:
+            follow_sets[nT[0]].add('#')
+            changed = True
+
+    return follow_sets
--- grammar/main.py
+++ grammar/main.py
@@ -1,4 +1,5 @@
 from generator_states import generateStateZero, generateStates
+from generator_sets import generateFirstSets, generateFollowSets
 
 # Grammar, Non-terminal symbols (uppercase), Terminal (all else)
 def import_grammar(fileData):
@@ -32,7 +33,6 @@ def main():
 
     # Grammar - list of rules (tuple: index, lhs, rhs)
     G, T, nT = import_grammar(fileData)
-
     # print(G)
     # print(T)
     # print(nT)
@@ -42,9 +42,16 @@ def main():
 
     # for s in states:
     #     print('State', s.orderNumber, '= goto(', s.prevStateOrderNumber, ',', s.gotoSymbol, ')', s.isCopy, s.copiedFromStateNumber)
-
     #     for t in s.transitions:            
     #         print('\t', t)
+
+    first_sets = generateFirstSets(G, T, nT)
+    for s in first_sets:
+        print(s, first_sets[s])
+
+    follow_sets = generateFollowSets(G, T, nT)
+    for s in follow_sets:
+        print(s, follow_sets[s])
 
 if __name__ == "__main__":
     main()
