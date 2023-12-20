96020e69101eabccb9fa1615962808781f3ee065
Irina Tomic <irina.z.tomic@gmail.com>
2023-12-20 11:15:36 +0100

    Start parser

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from loader.loader import open_file
 from lexer.lexer import lex
 from grammar.main import process_grammar
+from parser.parser import check_syntax
+
 
 def main():
-    file_path = "./examples/00.ench"
+
+    # Load
+    file_path = "./examples/simple/00.txt"
     file_contents = open_file(file_path)
 
     # Lexer
     words = lex(file_contents)
+    words = [word.word_type for word in words]
 
     # Generate grammar states
-    states = process_grammar()
+    grammar_path = "./grammar/example/grammar.txt"
+    states = process_grammar(grammar_path)
 
+    # Check syntax
+    # check_syntax(words, states)
 
 if __name__ == "__main__":
     main()
